edd0efd0b9e1febff1399a6a9f1cb81656fa7510
iamakbarilhamzxcv <iamakbarilham@gmail.com>
2025-02-17 10:28:59 +0700

    Update HomeStepDef.java

--- src/test/java/stepDefWeb/HomeStepDef.java
+++ src/test/java/stepDefWeb/HomeStepDef.java
@@ -15,7 +15,7 @@ public class HomeStepDef extends BaseTest {
 
     @Then("user will be redirect to home page")
     public void userWillBeRedirectToHomePage() throws InterruptedException {
-        Thread.sleep(10000);
+        Thread.sleep(15000);
         homePage = new HomePage(driver);
         homePage.validateOnHomePage();
     }
